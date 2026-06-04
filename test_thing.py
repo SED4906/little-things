98fdf1647798bc4a585d2b6e96c572e55dd8eb6f
SED4906 <sed4906birdie@gmail.com>
2026-06-03 19:40:01 -0700

    if you need me I'll be anywhere but here

--- test_thing.py
+++ test_thing.py
@@ -5,6 +5,11 @@ def can_drive(age):
 def test_16():
 	assert can_drive(16)
 
+def test_17():
+	assert can_drive(17)
+
 def test_15():
 	assert not can_drive(15)
 
+def test_14():
+	assert not can_drive(14)
